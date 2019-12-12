4a2135769239b64598a473c0fdb6d675215cc6e1
ceco851ba <ceco851ba@gmail.com>
2019-12-12 18:45:35 +0100

    commit_FINAL2

--- src/app/pages/statchart/statchart.page.ts
+++ src/app/pages/statchart/statchart.page.ts
@@ -27,7 +27,7 @@ export class StatchartPage implements OnInit {
   this.storage.get('transactions').then((val) => {
     this.TransactionsList = JSON.parse(val);
     this.TransactionsList.forEach(transaction => {
-      if (transaction.recieverId != null ){   ///Receiver ID null --> outgo transaction 
+      if (transaction.recieverId > transaction.senderId ){   ///Receiver ID null --> outgo transaction 
         
         this.TransactionsList.push(transaction);
           
